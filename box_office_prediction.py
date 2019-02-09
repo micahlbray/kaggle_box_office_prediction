27154f1ee5536746bde9f7af0b123c5b65c63678
micahlbray <micahlylebray@gmail.com>
2019-02-08 17:27:54 -0700

    In the middle of extracting dicts from columns in train_df. Goal is to create a separate file for each.

--- box_office_prediction.py
+++ box_office_prediction.py
@@ -5,6 +5,7 @@ Analyzes The Movie Database data set to predict box office revenue for a movie
 """
 
 import pandas as pd
+import json
 
 train_file = 'train.csv'
 test_file = 'test.csv'
@@ -12,5 +13,19 @@ test_file = 'test.csv'
 train_df = pd.read_csv(train_file)
 #test_df = pd.read_csv(test_file)
 
-print(train_df.dtypes)
-print(train_df.head())
+### train_df COLUMNS AS DICTS TO EXTRACT DATA FROM ###
+cols = ['belongs_to_collection', 'genres', 'production_companies', 'production_countries',
+        'spoken_languages', 'Keywords', 'cast', 'crew']
+for col in cols:
+    for i in range(len(train_df)):
+        #data = json.loads(train_df.loc[i, col])
+        data = train_df.loc[i, col]
+        try:
+            data = '"' + data[1:-1] + '"'
+            try:
+                data = json.loads(data)
+            except:
+                print("Could't convert to JSON")
+        except:
+            pass
+        print(data)
