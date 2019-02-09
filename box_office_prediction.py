e59552f79d112b94bce96f4fea3b019994670d92
micahlbray <micahlylebray@gmail.com>
2019-02-08 19:43:11 -0700

    Parsed columns as list and iterated through. Need to compile the parsed data into a useful dataframe and then create .csv files for each.

--- box_office_prediction.py
+++ box_office_prediction.py
@@ -6,6 +6,7 @@ Analyzes The Movie Database data set to predict box office revenue for a movie
 
 import pandas as pd
 import json
+import ast
 
 train_file = 'train.csv'
 test_file = 'test.csv'
@@ -13,19 +14,16 @@ test_file = 'test.csv'
 train_df = pd.read_csv(train_file)
 #test_df = pd.read_csv(test_file)
 
-### train_df COLUMNS AS DICTS TO EXTRACT DATA FROM ###
+### train_df COLUMNS AS LISTS WITH DICTS TO EXTRACT DATA FROM ###
 cols = ['belongs_to_collection', 'genres', 'production_companies', 'production_countries',
         'spoken_languages', 'Keywords', 'cast', 'crew']
 for col in cols:
-    for i in range(len(train_df)):
-        #data = json.loads(train_df.loc[i, col])
-        data = train_df.loc[i, col]
-        try:
-            data = '"' + data[1:-1] + '"'
-            try:
-                data = json.loads(data)
-            except:
-                print("Could't convert to JSON")
-        except:
-            pass
-        print(data)
+    train_df[cols] = train_df[cols].fillna('[]')
+    for i in range(1,5): #range(len(train_df)):
+        data = ast.literal_eval(train_df.loc[i, col])
+        if len(data) > 0:
+            for j in range(len(data)):
+                print(data[j])
+        else:
+            print(data) #should print empty list
+
