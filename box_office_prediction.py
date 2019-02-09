2111e6284681c08c1cba4ad9276278d78b5c9076
micahlbray <micahlylebray@gmail.com>
2019-02-09 08:56:36 -0700

    Parsed files and code for train_df and test_df.

--- box_office_prediction.py
+++ box_office_prediction.py
@@ -11,12 +11,11 @@ import ast
 train_file = 'train.csv'
 test_file = 'test.csv'
 
-train_df = pd.read_csv(train_file)
-#test_df = pd.read_csv(test_file)
-
-### train_df COLUMNS AS LISTS WITH DICTS TO EXTRACT DATA FROM ###
+### COLUMNS AS LISTS WITH DICTS TO EXTRACT DATA FROM ###
 cols = ['belongs_to_collection', 'genres', 'production_companies', 'production_countries',
         'spoken_languages', 'Keywords', 'cast', 'crew']
+
+train_df = pd.read_csv(train_file)
 for col in cols:
     print(col)
     out_df = pd.DataFrame()  # empty dataframe to fill
@@ -31,3 +30,17 @@ for col in cols:
                 out_df = out_df.append(row_dict, ignore_index=True)
     out_df.to_csv('train_' + col + '.csv', index=False)
 
+test_df = pd.read_csv(test_file)
+for col in cols:
+    print(col)
+    out_df = pd.DataFrame()  # empty dataframe to fill
+    test_df[col] = test_df[col].fillna('[]')
+    for i in range(len(test_df)):
+        movie_id = int(test_df.loc[i, 'id'])  # keep track of movie's id
+        data = ast.literal_eval(test_df.loc[i, col])
+        if len(data) > 0:
+            for j in range(len(data)):
+                row_dict = {'movie_id': movie_id}
+                row_dict.update(data[j])
+                out_df = out_df.append(row_dict, ignore_index=True)
+    out_df.to_csv('test_' + col + '.csv', index=False)
