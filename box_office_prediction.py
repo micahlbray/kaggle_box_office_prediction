af6ffb2979b9988acc532669e186a58ee0a1ce36
micahlbray <micahlylebray@gmail.com>
2019-02-08 21:33:33 -0700

    Parsed files and code.

--- box_office_prediction.py
+++ box_office_prediction.py
@@ -18,12 +18,16 @@ train_df = pd.read_csv(train_file)
 cols = ['belongs_to_collection', 'genres', 'production_companies', 'production_countries',
         'spoken_languages', 'Keywords', 'cast', 'crew']
 for col in cols:
-    train_df[cols] = train_df[cols].fillna('[]')
+    print(col)
+    out_df = pd.DataFrame()  # empty dataframe to fill
+    train_df[col] = train_df[col].fillna('[]')
     for i in range(len(train_df)):
+        movie_id = int(train_df.loc[i, 'id'])  # keep track of movie's id
         data = ast.literal_eval(train_df.loc[i, col])
         if len(data) > 0:
             for j in range(len(data)):
-                print(data[j])
-        else:
-            print(data) #should print empty list
+                row_dict = {'movie_id': movie_id}
+                row_dict.update(data[j])
+                out_df = out_df.append(row_dict, ignore_index=True)
+    out_df.to_csv('train_' + col + '.csv', index=False)
 
